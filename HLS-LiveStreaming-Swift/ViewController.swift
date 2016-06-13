551796ae15b0fa03dbfd1ebd7004eab488602a07
Alistair Cooper <cooper@swiftcodecomposer.com>
2016-06-13 00:49:23 -0700

    Initial Commit

---
+++ HLS-LiveStreaming-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HLS-LiveStreaming-Swift
+//
+//  Created by Alistair Cooper on 6/13/16.
+//  Copyright © 2016 Alistair Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
